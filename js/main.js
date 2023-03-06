47a7891fb24390e8dbc4f6117642263729ebef15
Abduvaliyev Alisher <94097356+Alisher003@users.noreply.github.com>
2023-03-06 14:10:16 +0500

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,13 +1,19 @@
 'user strick'
 
+const choices = ['rock', 'paper', 'scissors'];
+let randomIndex = ''
 let elBox = document.querySelector(".box-btns");
 let elPaperBtn = document.querySelector(".paper");
 let elScissorsBtn = document.querySelector(".scissors");
 let elRockBtn = document.querySelector(".rock");
 let elPlayBtn = document.querySelector(".play-btn");
-let elComputerResult = document.querySelector(".result-computer");
 let elGameResult = document.querySelector(".game-result");
 let elScore = document.querySelector(".navbar-btn__desc");
+var userValue = '';
+
+
+
+
 
 //Add class 
 elPlayBtn.style.display = "none"
@@ -21,8 +27,7 @@ elScore.textContent = score;
 
 // Computer choice function+++
 function getComputerChoice() {
-  const choices = ['rock', 'paper', 'scissors'];
-  const randomIndex = Math.floor(Math.random() * 3);
+  randomIndex = Math.floor(Math.random() * 3);
   if(choices[randomIndex] == "rock") {
     img.src = "img/rock.png";
     elBox.appendChild(img);
@@ -37,73 +42,84 @@ function getComputerChoice() {
 } 
 
 
+
+
 // UserChoise
   elPaperBtn.addEventListener("click",  function(){
     elPlayBtn.style.display = "block";
     elPlayBtn.classList.add("btn-fixed")
-    if(getComputerChoice() == "rock" ) {
-      // elGameResult.textContent = "YOU WIN";
-      elScore.textContent = score += 1;
-    }else if(getComputerChoice() == "scissors" ) {
-      // elGameResult.textContent = "YOU LOSE";
-      elScore.textContent = score -= 1;
-    }else if(getComputerChoice() == "paper") {
-      // elGameResult.textContent = "YOU DROW";
-    }
-    
+    userValue = document.getElementById('paper').value
     getComputerChoice();
     elScissorsBtn.style.display = "none";
     elRockBtn.style.display = "none";
+    winner();
   })
   
   elScissorsBtn.addEventListener("click", function(){
     elPlayBtn.style.display = "block";
     elPlayBtn.classList.add("btn-fixed")
-    if(getComputerChoice() == "paper") {
-      // elGameResult.textContent = "YOU WIN";
-      elScore.textContent = score += 1;
-    }else if(getComputerChoice() == "scissors") {
-      // elGameResult.textContent = "YOU DROW";
-    }else if(getComputerChoice() == "rock") {
-      // elGameResult.textContent = "YOU LOSE";
-      elScore.textContent = score -= 1;
-    }
+    userValue = document.getElementById('scissors').value
     getComputerChoice();
-
     elPaperBtn.style.display = "none";
     elRockBtn.style.display = "none";
+    winner();
   })
   
   elRockBtn.addEventListener("click", function(){
     elPlayBtn.style.display = "block";
     elPlayBtn.classList.add("btn-fixed")
     elRockBtn.classList.add("rock-fixed")
-    elRockBtn.classList.add("rock-fixed")
-    if(getComputerChoice() == "paper") {
-      // elGameResult.textContent = "YOU LOSE";
-      elScore.textContent = score -= 1;
-    }else if(getComputerChoice() == "scissors") {
-      // elGameResult.textContent = "YOU WIN";
-      elScore.textContent = score += 1;
-    }else if(getComputerChoice() == "rock") {
-      // elGameResult.textContent = "YOU DROW";
-    }
-    
     getComputerChoice();
     elPaperBtn.style.display = "none";
     elScissorsBtn.style.display = "none";
-  })
+    userValue = document.getElementById('rock').value
+    winner();
+    })
 
 
+
+  function winner(getcomputerChoice) {
+    if (userValue == choices[randomIndex]) {
+      console.log("drow");
+      elGameResult.textContent = "Drow";
+    } else if (userValue == 'paper' && choices[randomIndex] == 'scissors') {
+      console.log("lose");
+      elGameResult.textContent = "Lose";
+      elScore.textContent = score -= 1;
+    } else if (userValue == 'paper' && choices[randomIndex] == 'rock') {
+      console.log("win");
+      elGameResult.textContent = "Win";
+      elScore.textContent = score += 1;
+    } else if (userValue == 'scissors' && choices[randomIndex] == 'paper') {
+      console.log("win");
+      elGameResult.textContent = "Win";
+      elScore.textContent = score += 1;
+    } else if (userValue == 'scissors' && choices[randomIndex] == 'rock') {
+      console.log("lose");
+      elGameResult.textContent = "Lose";
+      elScore.textContent = score -= 1;
+    } else if (userValue == 'rock' && choices[randomIndex] == 'paper') {
+      console.log("lose");
+      elGameResult.textContent = "Lose";
+      elScore.textContent = score -= 1;
+    } else if (userValue == 'rock' && choices[randomIndex] == 'scissors') {
+      console.log("win");
+      elGameResult.textContent = "Win";
+      elScore.textContent = score += 1;
+    }
+  }
+
+
+  
 
 
 
   // restart game
   elPlayBtn.addEventListener("click", function(){
     elPlayBtn.style.display = "none";
-    elGameResult.style.display = "none";
+    elGameResult.textContent = "";
     img.src = " ";
     elScissorsBtn.style.display = "inline-block";
     elRockBtn.style.display = "inline-block";
-    elPaperBtn.style.display = "inline-block"
+    elPaperBtn.style.display = "inline-block";
   })
